98d670b7397c9b0b47e9093cceb751f6077b2947
K1laptop03 <K1laptop03>
2016-07-11 16:15:51 +0900

    first commit of mcrd.el

---
+++ mcrd.el
@@ -0,0 +1,57 @@
+;;; mcrd.el --- elisp for Macrodown                  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  
+
+;; Author:  <K1laptop03@K1LAPTOP03>
+;; Keywords:
+
+(provide 'mcrd)
+
+(defface mcrd-control-sequence-name-face
+  '((t (:foreground "#8888ff" :background "dark")))
+  "Macrodown control sequence")
+
+(defface mcrd-id-name-face
+  '((t (:foreground "#ff8844" :background "dark")))
+  "Macrodown ID name argument")
+
+(defface mcrd-class-name-face
+  '((t (:foreground "#ff4488" :background "dark")))
+  "Macrodown class name argument")
+
+(defface mcrd-var-in-string-face
+  '((t (:foreground "#44ff88" :background "dark")))
+  "Macrodown variable in string")
+
+(defface mcrd-escaped-character
+  '((t (:foreground "#cc88ff" :background "dark")))
+  "Macrodown escaped character")
+
+(defface mcrd-literal-area
+  '((t (:foreground "#ffff44" :background "dark")))
+  "Macrodown literal area")
+
+(define-generic-mode mcrd-mode
+  '(?%)
+
+  '("let" "let-mutable" "let-lazy" "in" "and"
+    "match" "with" "if" "then" "else"
+    "type" "of"
+    "module" "struct" "end-struct"
+    "before" "new-global-hash")
+
+  '(("\\(\\\\\\(?:\\\\\\\\\\)*[a-zA-Z\\-]+\\)\\>"
+     (1 'mcrd-control-sequence-name-face t))
+    ("\\(#[a-zA-Z\\-]+\\)\\>"
+     (1 'mcrd-id-name-face t))
+    ("\\(\\.[a-zA-Z\\-]+\\)\\>"
+     (1 'mcrd-class-name-face t))
+    ("\\(@[a-zA-Z\\-]+\\)\\>"
+     (1 'mcrd-var-in-string-face t))
+    ("\\(\\\\\\(?:@\\|`\\|\\*\\| \\|%\\||\\|;\\|{\\|}\\|\\\\\\)\\)"
+     (1 'mcrd-escaped-character t))
+    ("\\(`\\(?:[^`]\\|\\n\\)+`\\)"
+     (1 'mcrd-literal-area t)))
+
+  nil
+  '())
